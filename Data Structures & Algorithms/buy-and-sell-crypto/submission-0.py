0286e607a0be58979801447b9897c6adb736a681
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2026-06-19 00:59:19 +0600

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        i = 0
+        j = 1
+
+        while j < len(prices) :
+
+            if prices[i] > prices[j]:
+                i = j; 
+            else :
+                maxP = max(maxP, prices[j] - prices[i])
+
+            j+=1
+
+        
+ 
+        return  maxP
+        
